c0b53a4232022ad56d948a5209d79f129ea833a9
Douglas Starnes <douglas.a.starnes@gmail.com>
2016-02-22 17:31:05 +0000

    mystery files

--- config.py
+++
@@ -1,8 +0,0 @@
-class BaseConfig(object):
-    pass
-
-
-class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
-    DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = True # suppress warning
